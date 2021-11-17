a405f4582a74a50ce03801ef1cf7f800b6076c30
ibrahimhesenli <73495095+ibrahimhesenli@users.noreply.github.com>
2021-11-17 19:04:29 +0400

    Add files via upload

---
+++ healthy_future/js/script.js
@@ -0,0 +1,18 @@
+// The Slideshow Of App
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("app_image");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length} ;
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex-1].style.display = "block";
+}
